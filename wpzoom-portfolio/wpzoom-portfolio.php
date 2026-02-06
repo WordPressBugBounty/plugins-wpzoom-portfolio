d417269984490eb6495ca643ef743ee0842a9c70
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 18:26:49 +0000

    1.4.17

--- wpzoom-portfolio/wpzoom-portfolio.php
+++ wpzoom-portfolio/wpzoom-portfolio.php
@@ -14,7 +14,7 @@
  * Author:      WPZOOM
  * Author URI:  https://www.wpzoom.com
  * Text Domain: wpzoom-portfolio
- * Version:     1.4.16
+ * Version:     1.4.17
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -185,7 +185,7 @@ class WPZOOM_Blocks {
         wp_enqueue_script(
             'portfolio-masonry-editor',
             WPZOOM_PORTFOLIO_URL . 'assets/js/editor-init-masonry.js',
-            array('wp-edit-post', 'wp-dom-ready', 'masonry', 'imagesloaded'),
+            array('wp-data', 'wp-dom-ready', 'masonry', 'imagesloaded'),
             WPZOOM_PORTFOLIO_VERSION,
             true
         );
